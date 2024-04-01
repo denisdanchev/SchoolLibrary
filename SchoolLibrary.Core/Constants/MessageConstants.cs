c3c5f5924aa4a04aa199a00860b75bf9562a014f
Denis Danchev <denisdanchev982@gmail.com>
2024-04-01 19:40:16 +0300

    Message Constants added

--- SchoolLibrary.Core/Constants/MessageConstants.cs
+++ SchoolLibrary.Core/Constants/MessageConstants.cs
@@ -6,5 +6,7 @@
 
         public const string LengthMessage = "The field {0} must be between {2} and {1} characters long";
 
+        public const string AuthroNameExists = "Author name already exist";
+        public const string HasTakes = "You should have no teaked books to be an author!";
     }
 }
